a680fb75ac347f5995cd3ff086040988ea3cd4ec
Raphael Gomide <raphael.gomide@gmail.com>
2023-10-22 16:28:15 -0300

    update: latest Remix version

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -40,8 +40,7 @@ function NavigationItem({
         'border-b-2 px-1 pt-1 text-sm font-medium',
         isCurrentRoute
           ? 'border-indigo-500 text-gray-900'
-          : 'border-transparent text-gray-500',
-        'hover:border-gray-300 hover:text-gray-700'
+          : 'border-transparent text-gray-500, hover:border-gray-300 hover:text-gray-700'
       )}
     >
       {children}
