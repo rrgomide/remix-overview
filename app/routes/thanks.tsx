fd8d842ee021ee723bb776e73fcba4e8feb8fe30
Raphael Gomide <raphael.gomide@gmail.com>
2023-11-23 09:06:38 -0300

    chore: adjustments before presentation

--- app/routes/thanks.tsx
+++ app/routes/thanks.tsx
@@ -1,5 +1,7 @@
 import { json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
+import { GithubIcon } from '~/components'
+import { cn } from '~/utils'
 
 const inspirations = [
   {
@@ -43,6 +45,10 @@ const inspirations = [
     twitter: 'https://twitter.com/gugaguichard',
   },
   {
+    name: 'Daniel Weinmann',
+    twitter: 'https://twitter.com/danielweinmann',
+  },
+  {
     name: 'Brooks Lybrand',
     twitter: 'https://twitter.com/BrooksLybrand',
   },
@@ -68,24 +74,56 @@ export default function ThanksRoute() {
   const { inspirations } = useLoaderData<typeof loader>()
 
   return (
-    <div>
-      <h2 className="text-center font-semibold text-xl">
-        These people inspire me on keep learning Remix!
-      </h2>
+    <div className="w-full flex flex-row items-start justify-center mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="flex flex-col items-center justify-center">
+        <div
+          aria-label="Github Repository"
+          className="flex flex-row items-center justify-start space-x-2 hover:text-sky-600"
+        >
+          <GithubIcon />
+          <a
+            href="https://github.com/rrgomide/remix-overview"
+            target="_blank"
+            rel="noreferrer"
+            className={cn('text-lg')}
+          >
+            https://github.com/rrgomide/remix-overview
+          </a>
+        </div>
 
-      <ul className="mt-8 flex flex-col space-y-2">
-        {inspirations.map(({ name, twitter }) => (
-          <li key={name} className="hover:text-blue-600">
-            {twitter ? (
-              <a href={twitter} target="_blank" rel="noreferrer">
-                {name}
-              </a>
-            ) : (
-              name
-            )}
-          </li>
-        ))}
-      </ul>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/rrgomide/?locale=en_US"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="linkedin.jpeg"
+              alt="LinkedIn Profile"
+              aria-label="LinkedIn Profile"
+              className="w-96 h-96 rounded-md mt-12"
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-start justify-start ml-12 pl-8 border-l">
+        <h2 className="text-center font-semibold text-xl">Inspirations</h2>
+
+        <ul className="mt-8 flex flex-col space-y-2">
+          {inspirations.map(({ name, twitter }) => (
+            <li key={name} className="hover:text-sky-600">
+              {twitter ? (
+                <a href={twitter} target="_blank" rel="noreferrer">
+                  {name}
+                </a>
+              ) : (
+                name
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
